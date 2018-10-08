adf09a2ea21ee1cc0a17e137869487658a0f96cd
Ashish Kumar <42461980+ashish22panigrahi@users.noreply.github.com>
2018-10-08 20:18:57 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,24 @@
+package guvi_kids;
+import java.util.*;
+public class Number {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		double a=obj.nextDouble();
+		if(a >0.0) {
+			System.out.println("positive");
+		}
+		else if(a==0.0) {
+			System.out.println("zero");
+		}
+		else {
+			System.out.println("negative");
+			
+		}
+	}
+		
+		
+
+	}
+
+
